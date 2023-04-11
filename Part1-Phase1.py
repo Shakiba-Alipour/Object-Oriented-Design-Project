2e218e0482069af541c92b910f1b23a4b4b3888b
Shakiba <alipour.sh79@gmail.com>
2023-04-11 13:07:21 +0430

    read_zip_file is completed

--- Part1-Phase1.py
+++ Part1-Phase1.py
@@ -1,5 +1,5 @@
-import logging
 import re
+from click import File
 import plyj.parser as plyj
 import zipfile
 from io import BytesIO
@@ -17,11 +17,16 @@ def read_zip_file():
                 with zipfile.ZipFile(projectFile) as project:
                     for fileName in project.namelist():
                         # Now we can extract
-                        # logging.info("Found internal internal file: " + fileName)
                         if (fileName.endswith('.java')) & ('src' in fileName) & (not 'test' in fileName):
-                            file = project.open(fileName)
-                            print(fileName)
+                            file = File(project.open(fileName))
                             src.append([fileName, file])
 
 
+def parser():
+    parser = plyj.Parser()
+    tree = parser.parse_file(src[0][1])
+    print(tree)
+
+
 read_zip_file()
+# parser()
