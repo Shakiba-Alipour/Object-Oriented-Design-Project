0635ffa92fcad8122e05b1bb2bdee4e27b6c3025
Shakiba <alipour.sh79@gmail.com>
2023-04-11 12:15:42 +0430

    read_zip_file method is added

--- Part1-Phase1.py
+++ Part1-Phase1.py
@@ -1,1 +1,27 @@
-import javalang
+import logging
+import re
+import plyj.parser as plyj
+import zipfile
+from io import BytesIO
+
+src = []
+
+
+def read_zip_file():
+
+    with zipfile.ZipFile('Open Source OO project-20230329.zip', mode='r') as srczip:
+        for projectName in srczip.namelist():
+            if re.search(r'\.zip$', projectName) is not None:
+                # We have a zip within a zip
+                projectFile = BytesIO(srczip.read(projectName))
+                with zipfile.ZipFile(projectFile) as project:
+                    for fileName in project.namelist():
+                        # Now we can extract
+                        # logging.info("Found internal internal file: " + fileName)
+                        if (fileName.endswith('.java')) & ('src' in fileName) & (not 'test' in fileName):
+                            file = project.open(fileName)
+                            print(fileName)
+                            src.append([fileName, file])
+
+
+read_zip_file()
