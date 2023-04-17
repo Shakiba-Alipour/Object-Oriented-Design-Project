336889c7041f9a87149cc490ff6273b9ef64e24b
Shakiba <alipour.sh79@gmail.com>
2023-04-17 18:06:46 +0430

    Parser class is modified and some methods are added to it which parse a class or method.

--- Part1-Phase1/Part1-Phase1/src/main/java/Parser.java
+++ Part1-Phase1/Part1-Phase1/src/main/java/Parser.java
@@ -1,23 +1,32 @@
 import com.github.javaparser.*;
+import com.github.javaparser.ast.*;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
 
 public class Parser {
+
+    private File file;
     private JavaParser jp;
+    private ParseResult<CompilationUnit> cu;
     private String projectName;
-    private BufferedReader file;
+    private BufferedReader fileReader;
     private XSSFWorkbook workbook;
     private XSSFSheet sheet;
 
     public Parser(File file, String projectName, XSSFWorkbook workbook) throws FileNotFoundException {
+        this.file = file;
         this.projectName = projectName;
         this.jp = new JavaParser();
-        this.file = new BufferedReader(new FileReader(file));
-        jp.parse(file);
+        this.fileReader = new BufferedReader(new FileReader(file));
+        this.cu = jp.parse(file);
         this.workbook = workbook;
         //create sheet
         String[] completeFileName = file.getName().split(".");
@@ -142,12 +151,154 @@ public class Parser {
 
     private void parse() throws IOException {
 
+        // row 0 : project name
         Cell cell = this.sheet.createRow(0).createCell(1);
         cell.setCellValue(projectName);
-        while (true) {
-            String line = this.file.readLine();
 
+        this.fileReader = new BufferedReader(new FileReader(file));
+        try {
+            String line;
+            while ((line = this.fileReader.readLine()) != null) {
+                CompilationUnit cu = StaticJavaParser.parse(line);
+
+                class Visitor extends VoidVisitorAdapter<Void> {
+
+                    private XSSFSheet sheet;
+                    Cell cell;
+
+                    Visitor(XSSFSheet sheet) {
+                        this.sheet = sheet;
+                    }
+
+                    XSSFSheet getSheet() {
+                        return this.sheet;
+                    }
+
+                    @Override
+                    public void visit(FieldDeclaration fd, Void arg) {
+                        super.visit(fd, arg);
+                        System.out.println("Found field: " + fd.getVariables().get(0).getNameAsString() +
+                                " of type " + fd.getCommonType());
+                    }
+
+                    @Override
+                    public void visit(ClassOrInterfaceDeclaration cid, Void arg) {
+                        super.visit(cid, arg);
+
+                        // row 1: package name
+                        PackageDeclaration pd = cid.findAncestor(PackageDeclaration.class).orElse(null);
+                        if (pd != null) {
+                            cell = this.sheet.createRow(1).createCell(1);
+                            cell.setCellValue(pd.getNameAsString());
+                        }
+
+                        // row 2: class name
+                        cell = this.sheet.createRow(2).createCell(1);
+                        cell.setCellValue(cid.getNameAsString());
+
+                        // row 3: class type
+                        // row 8: check if it's an interface
+                        if (!cid.isInterface() && !cid.isNestedType()) {
+                            cell = this.sheet.createRow(3).createCell(1);
+                            cell.setCellValue("1");
+                        } else if (cid.isInterface()) {
+                            cell = this.sheet.createRow(3).createCell(1);
+                            cell.setCellValue("2");
+                            cell = this.sheet.createRow(8).createCell(1);
+                            cell.setCellValue("true");
+                        } else if (cid.isNestedType()) {
+                            cell = this.sheet.createRow(3).createCell(1);
+                            cell.setCellValue("3");
+                        }
+
+                        // row 4: class visibility
+                        if (cid.isPublic()) {
+                            cell = this.sheet.createRow(4).createCell(1);
+                            cell.setCellValue("1");
+                        } else if (cid.isPrivate()) {
+                            cell = this.sheet.createRow(4).createCell(1);
+                            cell.setCellValue("2");
+                        } else if (cid.isProtected()) {
+                            cell = this.sheet.createRow(4).createCell(1);
+                            cell.setCellValue("3");
+                        }
+
+                        // row 5 : class_is_abstract
+                        if (cid.isAbstract()) {
+                            cell = this.sheet.createRow(5).createCell(1);
+                            cell.setCellValue("1");
+                        } else {
+                            cell = this.sheet.createRow(5).createCell(1);
+                            cell.setCellValue("0");
+                        }
+
+                        // row 6 : class_is_static
+                        if (cid.isStatic()) {
+                            cell = this.sheet.createRow(6).createCell(1);
+                            cell.setCellValue("1");
+                        } else {
+                            cell = this.sheet.createRow(6).createCell(1);
+                            cell.setCellValue("0");
+                        }
+
+                        // row 7: check if the class is final
+                        if (cid.isFinal()) {
+                            cell = this.sheet.createRow(7).createCell(1);
+                            cell.setCellValue("true");
+                        } else {
+                            cell = this.sheet.createRow(7).createCell(1);
+                            cell.setCellValue("0");
+                        }
+
+                        // row 9: extends
+                        if (cid.getExtendedTypes().size() > 0) {
+                            cell = this.sheet.createRow(9).createCell(1);
+                            for (ClassOrInterfaceType extendedType : cid.getExtendedTypes()) {
+                                cell.setCellValue(cell.getStringCellValue() + ", " + extendedType.asString());
+                            }
+                        } else {
+                            cell.setCellValue("0");
+                        }
+
+                        // row 10: implemeny=ts
+                        if (cid.getImplementedTypes().size() > 0) {
+                            cell = this.sheet.createRow(10).createCell(1);
+                            for (ClassOrInterfaceType implementedType : cid.getImplementedTypes()) {
+                                cell.setCellValue(cell.getStringCellValue() + ", " + implementedType.asString());
+                            }
+                        } else {
+                            cell.setCellValue("0");
+                        }
+                    }
+
+                    // row 14
+                    @Override
+                    public void visit(MethodDeclaration md, Void arg) {
+                        super.visit(md, arg);
+
+                        String parameters = null;
+                        for (int i = 0; i < md.getParameters().size(); i++) {
+                            if (i > 0) {
+                                parameters += ", ";
+                            }
+                            parameters += md.getParameters().get(i).getTypeAsString();
+                        }
+
+                        cell.setCellValue(cell.getRichStringCellValue() + "\n" + md.getTypeAsString()
+                                + " " + md.getNameAsString() + " (" + parameters + ")");
+                    }
+                }
+
+
+                // Visit the AST with the visitor
+                Visitor visitor = new Visitor(this.sheet);
+                visitor.visit(cu, null);
+                this.sheet = visitor.getSheet();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 
 }
