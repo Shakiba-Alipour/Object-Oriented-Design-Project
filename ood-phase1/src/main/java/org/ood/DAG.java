c009267723d0a02d79b9c2ac663c45ea467533b5
Shakiba <alipour.sh79@gmail.com>
2023-05-24 19:56:51 +0330

    DAG class is created and add_node, add_edge and save methods are added to it

--- ood-phase1/src/main/java/org/ood/DAG.java
+++ ood-phase1/src/main/java/org/ood/DAG.java
@@ -22,24 +22,26 @@ public class DAG {
         this.graph = new SimpleGraph<>(DefaultEdge.class);
     }
 
-    public void add_nodes(String vertex) {
+    public void add_node(String vertex) {
         this.graph.addVertex(vertex);
     }
 
-    public void add_edges(String vertex1, String vertex2, int relationship) {
+    public void add_edge(String vertex1, String vertex2, int relationship) {
         DefaultEdge edge = this.graph.addEdge(vertex1, vertex2);
         this.graph.setEdgeWeight(edge, relationship);
     }
 
-    public void save() throws IOException {
+    public void save(String class_name) throws IOException {
         JGraphXAdapter<Object, DefaultEdge> graphAdapter = new JGraphXAdapter<>(this.graph);
         mxIGraphLayout layout = new mxCircleLayout(graphAdapter);
         layout.execute(graphAdapter.getDefaultParent());
         BufferedImage image = mxCellRenderer.createBufferedImage(graphAdapter, null, 2,
                 new Color(0f, 0f, 0f, .5f), true, null);
-        File png_image = new File("src/test/java/graph.png");
+        String png_path = "src/test/java/" + class_name + ".png";
+        File png_image = new File(png_path);
         ImageIO.write(image, "PNG", png_image);
-        File jpg_image = new File("src/test/java/graph.jpg");
+        String jpg_path = "src/test/java/" + class_name + ".jpg";
+        File jpg_image = new File(jpg_path);
         ImageIO.write(image, "JPG", jpg_image);
     }
 
